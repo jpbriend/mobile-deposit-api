18c0a65a76e7eb6616c1ab215a8cbf477db5ed2e
Nigel Harniman <harniman@users.noreply.github.com>
2016-01-29 11:44:47 +0000

    Fixing host ip

--- flow.groovy
+++ flow.groovy
@@ -36,7 +36,7 @@ node('docker') {
         matcher = null
     }
 
-    docker.withServer('tcp://192.168.99.102:2376', 'slave-docker-us-east-1-tls'){
+    docker.withServer('tcp://192.168.99.100:2376', 'slave-docker-us-east-1-tls'){
 
         stage 'Build Docker image'
         def mobileDepositApiImage
